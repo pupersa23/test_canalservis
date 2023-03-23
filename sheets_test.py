27cad65db9a816469a1280dc5344706759dcadaf
Владимир Рязанов <ryazanov745@gmail.com>
2023-03-23 22:23:13 +0300

    Подготовил полностью файл

--- sheets_test.py
+++ sheets_test.py
@@ -1,19 +1,53 @@
 import datetime as dt
+import logging
 import os
+import sys
+import time
 
 import gspread
 import psycopg2
 from dotenv import load_dotenv
 from pycbrf import ExchangeRates
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s, %(name)s, %(levelname)s, %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler(sys.stdout)])
+logger = logging.getLogger(__name__)
+
 load_dotenv()
 
 GOOGLEAPIJSON = os.getenv('GOOGLEAPIJSON')
 SHEETID = os.getenv('SHEETID')
+
 DBNAME = os.getenv('DBNAME')
-USER_B = os.getenv('USERNAME')
+DBNAME_2 = os.getenv('DBNAME_2')
+USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 HOST = os.getenv('HOST')
+TABLE_NAME = 'delivery'
+
+RETRY_TIME = 30
+
+
+def connect_to_maindb(value):
+    """Выбор подключения к БД"""
+    if value == DBNAME:
+        conn = psycopg2.connect(
+            dbname=DBNAME,
+            user=USERNAME,
+            password=PASSWORD,
+            host=HOST,
+        )
+    else:
+        conn = psycopg2.connect(
+            dbname=DBNAME_2,
+            user=USERNAME,
+            password=PASSWORD,
+            host=HOST,
+        )
+    return conn
 
 
 def get_sheet():
@@ -25,6 +59,36 @@ def get_sheet():
     worksheet = sht1.get_worksheet(0)
     list_of_dicts = worksheet.get_all_records()
     return list_of_dicts
+
+
+def create_db():
+    """Создание БД и добавление таблицы"""
+    conn = connect_to_maindb(DBNAME)
+    cursor = conn.cursor()
+    conn.autocommit = True
+    cursor.execute(
+        f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{DBNAME_2}'"
+    )
+    exists = cursor.fetchone()
+    if not exists:
+        cursor.execute(f"CREATE DATABASE {DBNAME_2}")
+        logger.info('База данных создана')
+        conn.close()
+
+    conn_2 = connect_to_maindb(DBNAME_2)
+    cursor = conn_2.cursor()
+    conn_2.autocommit = True
+    cursor.execute(
+        f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME}
+                (id INTEGER PRIMARY KEY,
+                 order_number INTEGER,
+                 price_dollar INTEGER,
+                 price_ruble INTEGER,
+                 date DATE)"""
+    )
+    logger.info('Таблица создана')
+    cursor.close()
+    conn_2.close()
 
 
 def convert(value):
@@ -42,16 +106,11 @@ def add_date(value):
         new_date = dt.datetime.strptime(date1, "%d.%m.%Y")
         newdate = new_date.strftime("%Y-%m-%d")
 
-        conn = psycopg2.connect(
-            dbname=DBNAME,
-            user=USER_B,
-            password=PASSWORD,
-            host=HOST,
-        )
-        cursor = conn.cursor()
-        conn.autocommit = True
+        conn_2 = connect_to_maindb(DBNAME_2)
+        cursor = conn_2.cursor()
+        conn_2.autocommit = True
         cursor.execute(
-            f"""INSERT INTO delivery
+            f"""INSERT INTO {TABLE_NAME}
             (id, order_number, price_dollar, price_ruble, date)
             VALUES ({list['№']},
                     {list['заказ №']},
@@ -59,10 +118,40 @@ def add_date(value):
                     {convert(list['стоимость,$'])},
                     '{newdate}'::date)"""
         )
+        logger.info('Данные добавлены в БД')
         cursor.close()
-        conn.close()
+        conn_2.close()
+
+
+def delet_old_date():
+    """Удаление старой таблицы"""
+    conn_2 = connect_to_maindb(DBNAME_2)
+    cursor = conn_2.cursor()
+    conn_2.autocommit = True
+    cursor.execute(f"DROP TABLE IF EXISTS {TABLE_NAME}")
+    logger.info('Таблица удалена')
+    cursor.close()
+    conn_2.close()
+
+
+def main():
+    """Основная работа программы"""
+    logger.info('Программа запущен')
+    current_timestamp = int(time.time())
+    while True:
+        try:
+            delet_old_date()
+            create_db()
+            list_of_dicts = get_sheet()
+            add_date(list_of_dicts)
+        except Exception as error:
+            current_timestamp = current_timestamp
+            logger.info(f'Сбой в работе программы: {error}')
+        else:
+            current_timestamp = int(time.time())
+        finally:
+            time.sleep(RETRY_TIME)
 
 
 if __name__ == "__main__":
-    list_of_dicts = get_sheet()
-    add_date(list_of_dicts)
+    main()
